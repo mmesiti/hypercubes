ccabe83783a5b1542d803ee5650238c6e5afd562
Michele Mesiti <michele.mesiti@gmail.com>
2021-04-29 16:59:45 +0200

    Ported tree.py to c++
    100% coverage by unit tests.

--- cpp/include/tree.hpp
+++ cpp/include/tree.hpp
@@ -4,7 +4,9 @@
 #include <functional>
 #include <memory>
 #include <ostream>
+#include <set>
 #include <sstream>
+#include <stdexcept>
 #include <tuple>
 #include <vector>
 
@@ -31,6 +33,30 @@ template <class Node> struct Tree {
     return false;
   }
   bool operator==(const Tree &other) const { return not(*this != other); };
+  bool operator<(const Tree &other) {
+    if (n < other.n)
+      return true;
+    for (int i = 0;                                         //
+         i < children.size() and i < other.children.size(); //
+         ++i)
+      if (*children[i] < *other.children[i])
+        return true;
+    if (children.size() < other.children.size())
+      return true;
+    return false;
+  }
+};
+
+template <class Node> struct less {
+
+  bool operator()(const TreeP<Node> &t1, //
+                  const TreeP<Node> &t2) {
+    return *t1 < *t2;
+  }
+  bool operator()(const Tree<Node> &t1, //
+                  const Tree<Node> &t2) {
+    return t1 < t2;
+  }
 };
 
 template <class Node> TreeP<Node> mt(Node n, vector<TreeP<Node>> v) {
@@ -111,7 +137,7 @@ template <class Node> int get_max_depth(TreeP<Node> tree) {
 }
 
 template <class Node> TreeP<Node> truncate_tree(TreeP<Node> tree, int level) {
-  if (level > 1) {
+  if (level > 0) {
     vector<TreeP<Node>> children;
     std::transform(tree->children.begin(), tree->children.end(),
                    std::back_inserter(children), [level](TreeP<Node> c) {
@@ -146,6 +172,126 @@ TreeP<std::tuple<Node, Nodes...>> ziptree(TreeP<Node> t, TreeP<Nodes>... ts) {
   for (int i = 0; i < t->children.size(); ++i)
     children.push_back(ziptree(t->children[i], ts->children[i]...));
   return mt(n, children);
+}
+
+template <class Node, class NewNode>
+TreeP<NewNode> nodemap(TreeP<Node> t, std::function<NewNode(Node)> f) {
+  vector<TreeP<NewNode>> children;
+  std::transform(t->children.begin(), t->children.end(),
+                 std::back_inserter(children),
+                 [&f](TreeP<Node> c) { return nodemap(c, f); });
+  return mt(f(t->n), children);
+}
+
+template <class Node>
+TreeP<Node> collapse_level(TreeP<Node> tree, int level_to_collapse,
+                           int child_to_replace) {
+
+  if (level_to_collapse == 0)
+    return tree->children[child_to_replace];
+  else {
+    vector<TreeP<Node>> children;
+    std::transform(tree->children.begin(), tree->children.end(),
+                   std::back_inserter(children),
+                   [level_to_collapse, child_to_replace](TreeP<Node> c) {
+                     return collapse_level(c, level_to_collapse - 1,
+                                           child_to_replace);
+                   });
+    return mt(tree->n, children);
+  }
+}
+
+template <class Node>
+TreeP<Node> bring_level_on_top(TreeP<Node> tree, int level) {
+
+  using TreeP = TreeP<Node>;
+  int nchildren = [&tree, level]() {
+    TreeP top_tree = truncate_tree(tree, level + 1);
+    vector<TreeP> subtrees = get_flat_list_of_subtrees(top_tree, level);
+    int nchildren = get_leaves_list(subtrees[0]).size();
+    for (const auto &st : subtrees)
+      if (nchildren != get_leaves_list(st).size())
+        throw std::invalid_argument(
+            "Not all subtrees have the same number of children.");
+    return nchildren;
+  }();
+
+  Node new_top = [&tree, level]() {
+    TreeP top_tree = truncate_tree(tree, level);
+    vector<Node> leaves = get_leaves_list(top_tree);
+    for (const auto &l : leaves)
+      if (l != leaves[0])
+        throw std::invalid_argument(
+            "Not all nodes are equal on the required level.");
+    return leaves[0];
+  }();
+
+  vector<TreeP> new_subtrees;
+  for (int child_to_pick = 0;     //
+       child_to_pick < nchildren; //
+       ++child_to_pick)
+    new_subtrees.push_back(collapse_level(tree, level, child_to_pick));
+  return mt(new_top, new_subtrees);
+}
+
+template <class Node>
+TreeP<Node> swap_levels(TreeP<Node> tree, vector<int> new_level_ordering) {
+
+  if (new_level_ordering.size() == 0)
+    return tree;
+
+  int next_level = new_level_ordering[0];
+  TreeP<Node> new_tree = bring_level_on_top(tree, next_level);
+
+  vector<int> sub_level_ordering = [&new_level_ordering]() {
+    vector<int> res;
+    int lvl_removed = new_level_ordering[0];
+    for (int i = 1; i < new_level_ordering.size(); ++i) {
+      int l = new_level_ordering[i];
+      if (l > lvl_removed)
+        res.push_back(l - 1);
+      else
+        res.push_back(l);
+    }
+    return res;
+  }();
+
+  vector<TreeP<Node>> new_children;
+  std::transform(new_tree->children.begin(),       //
+                 new_tree->children.end(),         //
+                 std::back_inserter(new_children), //
+                 [&sub_level_ordering](TreeP<Node> c) {
+                   return swap_levels(c, sub_level_ordering);
+                 });
+  return mt(new_tree->n, new_children);
+}
+
+template <class Node> vector<Node> first_nodes_list(TreeP<Node> tree) {
+
+  vector<Node> res;
+  auto _first_nodes_list = [](const TreeP<Node> subtree, vector<Node> &r,
+                              auto &f) -> void {
+    r.push_back(subtree->n);
+    if (subtree->children.size() != 0)
+      f(subtree->children[0], r, f);
+  };
+
+  _first_nodes_list(tree, res, _first_nodes_list);
+  return res;
+}
+
+template <class Node> vector<Node> depth_first_flatten(TreeP<Node> tree) {
+
+  vector<Node> res;
+  auto _depth_first_flatten = [](const TreeP<Node> subtree, vector<Node> &r,
+                                 auto &f) -> void {
+    r.push_back(subtree->n);
+    for (const auto &c : subtree->children)
+      f(c, r, f);
+  };
+
+  _depth_first_flatten(tree, res, _depth_first_flatten);
+  return res;
 }
 
 } // namespace slow
--- cpp/tests/test_tree.cpp
+++ cpp/tests/test_tree.cpp
@@ -1,6 +1,8 @@
 
+#include <boost/test/tools/old/interface.hpp>
 #include <boost/test/unit_test.hpp>
 #include <boost/test/unit_test_suite.hpp>
+#include <sstream>
 
 #include "tree.hpp"
 
@@ -101,7 +103,7 @@ BOOST_AUTO_TEST_CASE(test_truncate_tree) {
                                              mt(13, {})}),
                                   });
 
-  BOOST_TEST(*truncated_expected == *truncate_tree(t, 3));
+  BOOST_TEST(*truncated_expected == *truncate_tree(t, 2));
 }
 
 BOOST_AUTO_TEST_CASE(test_get_leaves_list) {
@@ -136,6 +138,163 @@ BOOST_AUTO_TEST_CASE(test_ziptree) {
 
   bool check = *tripled == *tripled_expected;
   BOOST_TEST(check);
+}
+
+BOOST_AUTO_TEST_CASE(test_nodemap) {
+
+  TreeP<int> t = mt(1, {mt(2, {}),                     //
+                        mt(3, {mt(4, {}), mt(5, {})}), //
+                        mt(6, {mt(7, {mt(8, {})})})});
+
+  std::function<std::string(int)> f = [](int i) {
+    std::stringstream ss;
+    ss << "N: ";
+    ss << i * 3;
+    return ss.str();
+  };
+
+  auto s = [](const char *st) { return std::string(st); };
+  auto expt =
+      mt(s("N: 3"), {mt(s("N: 6"), {}),                                       //
+                     mt(s("N: 9"), {mt(s("N: 12"), {}), mt(s("N: 15"), {})}), //
+                     mt(s("N: 18"), {mt(s("N: 21"), {mt(s("N: 24"), {})})})});
+
+  auto newt = nodemap(t, f);
+  BOOST_TEST(*expt == *newt);
+}
+
+BOOST_AUTO_TEST_CASE(test_collapse_level) {
+
+  auto t = mt(1, {
+                     mt(2, {mt(3, {mt(4, {}),   //
+                                   mt(5, {})}), //
+                            mt(6, {mt(7, {}),   //
+                                   mt(8, {})})}),
+                     mt(9, {mt(10, {mt(11, {}),   //
+                                    mt(12, {})}), //
+                            mt(13, {mt(14, {}),   //
+                                    mt(15, {})})}),
+                 });
+
+  auto tcollapsed0_exp = mt(1, {
+                                   mt(3, {mt(4, {}),     //
+                                          mt(5, {})}),   //
+                                   mt(10, {mt(11, {}),   //
+                                           mt(12, {})}), //
+                               });
+
+  auto tcollapsed0 = collapse_level(t,                       //
+                                    1 /*level to collapse*/, //
+                                    0 /*child to replace*/);
+
+  BOOST_TEST(*tcollapsed0_exp == *tcollapsed0);
+}
+
+BOOST_AUTO_TEST_CASE(test_bring_level_on_top_noop) {
+  auto t = mt(1, {
+                     mt(2, {mt(3, {mt(4, {}), //
+                                   mt(5, {})})}),
+                     mt(9, {mt(3, {mt(11, {}), //
+                                   mt(12, {})})}),
+                 });
+
+  auto tcopy = bring_level_on_top(t, 0);
+  BOOST_TEST(*t == *tcopy);
+}
+
+BOOST_AUTO_TEST_CASE(test_bring_level_on_top) {
+  auto t = mt(1, {
+                     mt(2, {mt(3, {mt(4, {}), //
+                                   mt(5, {})})}),
+                     mt(9, {mt(3, {mt(11, {}), //
+                                   mt(12, {})})}),
+                 });
+
+  auto transf_expected = mt(3, {mt(1, {mt(2, {mt(4, {})}), //
+                                       mt(9, {mt(11, {})})}),
+                                mt(1, {mt(2, {mt(5, {})}), //
+                                       mt(9, {mt(12, {})})})});
+
+  auto transf = bring_level_on_top(t, 2);
+
+  BOOST_TEST(*transf_expected == *transf);
+}
+
+BOOST_AUTO_TEST_CASE(test_bring_level_on_top_wrong_nodes) {
+  auto t = mt(1, {
+                     mt(2, {mt(3, {mt(4, {}), //
+                                   mt(5, {})})}),
+                     mt(9, {mt(0, {mt(11, {}), //
+                                   mt(12, {})})}),
+                 });
+  BOOST_CHECK_THROW(bring_level_on_top(t, 2), std::invalid_argument);
+}
+
+BOOST_AUTO_TEST_CASE(test_bring_level_on_top_wrong_children) {
+  auto t = mt(1, {
+                     mt(2, {mt(3, {mt(4, {}), //
+                                   mt(5, {})})}),
+                     mt(9, {mt(3, {mt(11, {})})}),
+                 });
+  BOOST_CHECK_THROW(bring_level_on_top(t, 2), std::invalid_argument);
+}
+
+BOOST_AUTO_TEST_CASE(test_swap_levels_noop) {
+
+  auto t = mt(1, {mt(2, {mt(3, {mt(5, {}),     //
+                                mt(6, {})})}), //
+                  mt(4, {mt(3, {mt(5, {}),     //
+                                mt(6, {})})})});
+  vector<int> new_level_ordering{0, 1, 2};
+
+  auto newt = swap_levels(t, new_level_ordering);
+
+  BOOST_TEST(*newt == *t);
+}
+
+BOOST_AUTO_TEST_CASE(test_swap_levels) {
+
+  auto t = mt(1, {mt(2, {mt(3, {mt(5, {}),     //
+                                mt(6, {})})}), //
+                  mt(4, {mt(3, {mt(7, {}),     //
+                                mt(8, {})})})});
+  vector<int> new_level_ordering{2, 0, 1};
+
+  auto newt_exp = mt(3, {mt(1, {mt(2, {mt(5, {})}),   //
+                                mt(4, {mt(7, {})})}), //
+                         mt(1, {mt(2, {mt(6, {})}),   //
+                                mt(4, {mt(8, {})})})});
+
+  auto newt = swap_levels(t, new_level_ordering);
+
+  BOOST_TEST(*newt == *newt_exp);
+}
+
+BOOST_AUTO_TEST_CASE(test_first_nodes_list) {
+
+  auto t = mt(1, {mt(2, {mt(3, {mt(5, {}),     //
+                                mt(6, {})})}), //
+                  mt(4, {mt(3, {mt(7, {}),     //
+                                mt(8, {})})})});
+
+  vector<int> first_nodes_expected{1, 2, 3, 5};
+
+  vector<int> first_nodes = first_nodes_list(t);
+
+  BOOST_TEST(first_nodes == first_nodes_expected);
+}
+
+BOOST_AUTO_TEST_CASE(test_depth_first_flatten) {
+
+  auto t = mt(1, {mt(2, {mt(3, {mt(5, {}),     //
+                                mt(6, {})})}), //
+                  mt(4, {mt(3, {mt(7, {}),     //
+                                mt(8, {})})})});
+
+  vector<int> dfflat_expected{1, 2, 3, 5, 6, 4, 3, 7, 8};
+  vector<int> dfflat = depth_first_flatten(t);
+
+  BOOST_TEST(dfflat_expected == dfflat);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
