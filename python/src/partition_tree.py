27804b90f74d9e6cfd9c83ebbbe60f73b096d920
Michele Mesiti <michele.mesiti@gmail.com>
2021-05-07 02:36:29 +0200

    Simplified iterate

--- python/src/partition_tree.py
+++ python/src/partition_tree.py
@@ -53,21 +53,6 @@ def get_start_tree(size_tree):
     return _start_tree(size_tree, 0)
 
 
-def get_start_idx(size_tree):
-    '''
-    Return first (depth-first-wise) indices list
-    in a tree.
-    '''
-    (s, idx), children = size_tree
-
-    def get_idx(subtree):
-        (_1, idx), _2 = subtree
-        return idx
-
-    if children == ():
-        return (0, )
-    else:
-        return (get_idx(children[0]), ) + get_start_idx(children[0])
 
 
 def iterate(size_tree, idxs):
@@ -79,29 +64,39 @@ def iterate(size_tree, idxs):
         (_1, idx), _2 = subtree
         return idx
 
-    def _iterate(size_tree, idxs):
-        (s, idx), children = size_tree
-        nidx = idxs[0]
+    def get_start_idx(subtree):
+        '''
+        Return first (depth-first-wise) indices list
+        in a tree.
+        '''
+        (s, idx), children = subtree
 
-        if children == ():  # Leaf case
-            new_idx = nidx + 1
-            if new_idx == s:
+        if children == ():
+            return (0, )
+        else:
+            return (get_idx(children[0]), ) + get_start_idx(children[0])
+
+
+    (s, idx), children = size_tree
+    nidx = idxs[0]
+
+    if children == ():  # Leaf case
+        new_idx = nidx + 1
+        if new_idx == s:
+            return None
+        else:
+            return (nidx + 1, )
+    else:  # Node case
+        # looking for child with the right idx
+        i, c = next(
+            (i, c) for i, c in enumerate(children) if get_idx(c) == nidx)
+
+        sub_idxs = iterate(c, idxs[1:])
+        if sub_idxs is None:  # We need to move to the next child
+            if i == len(children) - 1:  # We give up
                 return None
             else:
-                return (nidx + 1, )
-        else:  # Node case
-            # looking for child with the right idx
-            i, c = next(
-                (i, c) for i, c in enumerate(children) if get_idx(c) == nidx)
-
-            sub_idxs = _iterate(c, idxs[1:])
-            if sub_idxs is None:  # We need to move to the next child
-                if i == len(children) - 1:  # We give up
-                    return None
-                else:
-                    sub_idxs = get_start_idx(children[i + 1])
-                    return (i + 1, ) + sub_idxs
-            else:
-                return (i, ) + sub_idxs
-
-    return _iterate(size_tree, idxs)
+                sub_idxs = get_start_idx(children[i + 1])
+                return (i + 1, ) + sub_idxs
+        else:
+            return (i, ) + sub_idxs
